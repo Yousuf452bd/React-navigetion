2636370566bdecb1dd76f0683029275760f1d50f
Yousuf Ali <yousuf452bd@gmail.com>
2024-10-10 18:30:43 +0600

    Profile setction add

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,11 +4,11 @@ function Contact(){
 
     return(
         <>         <section>
-        <div class="container">
+                 <div class="container">
                 <div className="row ">
                     <div className="col-12 col-m-4 col-lg-3">
                         <h1 class="text-info"> Contact page</h1>
-                        <p>Lorem ipsum dolor is sit amet consectetur adipisicing elit. Quasi dolore, eius ut voluptatem dolorum quos perspiciatis. Similique adipisci at ipsam.</p>
+                        <p>Lorem ipsum dolor is sid amet consectetur adipisicing elit. Quasi dolore, eius ut voluptatem dolorum quos perspiciatis. Similique adipisci at ipsam.</p>
 
                     </div>
                     <div className="col-12 col-m-4 col-lg-3">
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,14 +4,33 @@ function Profile(){
 
     return(
         <>
-           <section>
-        <div class="container">
+            <section>
+                 <div class="container">
                 <div className="row ">
-                    <div className="col-12 col-m-4 col-lg-4">
-                        <h1 class="text-primary"> Profile page</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dolore, eius ut voluptatem dolorum quos perspiciatis. Similique adipisci at ipsam.</p>
+                    <div className="col-12 col-m-4 col-lg-3">
+                        <h1 class="text-info"> Contact page</h1>
+                        <p>Lorem ipsum dolor is sit amet consectetur adipisicing elit. Quasi dolore, eius ut voluptatem dolorum quos perspiciatis. Similique adipisci at ipsam.</p>
 
                     </div>
+                    <div className="col-12 col-m-4 col-lg-3">
+                        <h1 class="text-info"> Home page</h1>
+                        <p>Lorem ipsum dolor is sit amet consectetur adipisicing elit. Quasi dolore, eius ut voluptatem dolorum quos perspiciatis. Similique adipisci at ipsam.</p>
+
+                    </div>
+                    <div className="col-12 col-m-4 col-lg-3">
+                        <h1 class="text-info"> Home page</h1>
+                        <p>Lorem ipsum dolor is sit amet consectetur adipisicing elit. Quasi dolore, eius ut voluptatem dolorum quos perspiciatis. Similique adipisci at ipsam.</p>
+
+                    </div>
+                    <div className="col-12 col-m-4 col-lg-3">
+                        <h1 class="text-info"> Home page</h1>
+                        <p>Lorem ipsum dolor is sit amet consectetur adipisicing elit. Quasi dolore, eius ut voluptatem dolorum quos perspiciatis. Similique adipisci at ipsam.</p>
+
+                    </div>
+
+                  
+
+                    
                 </div>
         </div>
         </section>
