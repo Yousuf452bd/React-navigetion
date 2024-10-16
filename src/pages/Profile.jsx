e747578759c5352f4d1d1ff3708f0dab2fc0876f
Yousuf Ali <yousuf452bd@gmail.com>
2024-10-16 22:13:03 +0600

    profile add title

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -8,22 +8,22 @@ function Profile(){
                  <div class="container">
                 <div className="row ">
                     <div className="col-12 col-m-4 col-lg-3">
-                        <h1 class="text-info"> Contact page</h1>
+                        <h1 class="text-info"> Profile page</h1>
                         <p>Lorem ipsum dolor is sit amet consectetur adipisicing elit. Quasi dolore, eius ut voluptatem dolorum quos perspiciatis. Similique adipisci at ipsam.</p>
 
                     </div>
                     <div className="col-12 col-m-4 col-lg-3">
-                        <h1 class="text-info"> Home page</h1>
+                        <h1 class="text-info"> Profile page</h1>
                         <p>Lorem ipsum dolor is sit amet consectetur adipisicing elit. Quasi dolore, eius ut voluptatem dolorum quos perspiciatis. Similique adipisci at ipsam.</p>
 
                     </div>
                     <div className="col-12 col-m-4 col-lg-3">
-                        <h1 class="text-info"> Home page</h1>
+                        <h1 class="text-info"> Profile page</h1>
                         <p>Lorem ipsum dolor is sit amet consectetur adipisicing elit. Quasi dolore, eius ut voluptatem dolorum quos perspiciatis. Similique adipisci at ipsam.</p>
 
                     </div>
                     <div className="col-12 col-m-4 col-lg-3">
-                        <h1 class="text-info"> Home page</h1>
+                        <h1 class="text-info"> Profile page</h1>
                         <p>Lorem ipsum dolor is sit amet consectetur adipisicing elit. Quasi dolore, eius ut voluptatem dolorum quos perspiciatis. Similique adipisci at ipsam.</p>
 
                     </div>
---
+++ src/topBar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+ 
+
+export default class topBar extends Component {
+  render() {
+    return (
+      <div> hellotopBar</div>
+    )
+  }
+}
+ 
